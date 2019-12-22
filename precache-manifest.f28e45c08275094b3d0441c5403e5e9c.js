c97c4e09ba1e4ed4dc02f9f334d4e019a8260e92
debbyji <jasmine820921@gmail.com>
2019-12-22 15:21:46 +0800

    Updates

---
+++ precache-manifest.f28e45c08275094b3d0441c5403e5e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012f7b79d1a80e4c1cd75efe177fcc1e",
+    "url": "/xmas_exchange/index.html"
+  },
+  {
+    "revision": "da5055336b56d1fd7e49",
+    "url": "/xmas_exchange/static/css/main.e8240efd.chunk.css"
+  },
+  {
+    "revision": "63a22c98cd95e5c5a62c",
+    "url": "/xmas_exchange/static/js/2.2b601f37.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/xmas_exchange/static/js/2.2b601f37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da5055336b56d1fd7e49",
+    "url": "/xmas_exchange/static/js/main.2aef9120.chunk.js"
+  },
+  {
+    "revision": "dd57f737d5c42a44d2e5",
+    "url": "/xmas_exchange/static/js/runtime-main.40dbf353.js"
+  }
+]);
