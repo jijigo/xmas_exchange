002c1f718c5e9c1f3faae9668a945693605bf0c4
debbyji <jasmine820921@gmail.com>
2019-12-23 02:06:21 +0800

    update meta, add ga code

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -5,7 +5,6 @@ function Step2 (props) {
 	const attendee = props.attendee
 
 	function handleChange(obj) {
-		console.log('obj', obj)
 		return (event) => props.setAttendee(attendee.map((o) => {
 			if (o === obj) return {...obj, name: event.target.value}
 			return o;
